a53d6ca71252479fb3fad5f2e432f706f88bac58
Kabbier <balinagkabbier@gmail.com>
2024-03-14 12:58:58 +0530

    Form added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
-<<<<<<< HEAD
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-=======
+import "./App.css";
 import End from "./Components/End";
 import FaceScanComponent from "./Components/FaceScanComponent";
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 import PersonalInfo from "./Components/PersonalInfo";
 import Scancard from "./Components/Scancard";
 import Welcomepage from "./Components/Welcomepage";
 
-
->>>>>>> 5542d4bb3dec233764788825746cc26fa07c9b23
-
-
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-<<<<<<< HEAD
-          <Route  path="/" element={<Home />} />
-=======
-          <Route  path="/scan" element={<FaceScanComponent />} />
-          <Route  path="/home" element={<Home />} />
-          <Route  path="/" element={<Welcomepage />} />
-          <Route  path="/form" element={<PersonalInfo />} />
-          <Route  path="/end" element={<End />} />
-          <Route  path="/scancard" element={<Scancard />} />
-
-
-
->>>>>>> 5542d4bb3dec233764788825746cc26fa07c9b23
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/scan" element={<FaceScanComponent />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Welcomepage />} />
+          <Route path="/form" element={<PersonalInfo />} />
+          <Route path="/end" element={<End />} />
+          <Route path="/scancard" element={<Scancard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
